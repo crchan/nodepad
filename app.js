e08193d6b9150df4eda821300caa66b8584c2568
ChongRyol Lee <crchan@nate.com>
2012-01-20 17:58:05 +0900

    node-tutorial-5

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
  * Module dependencies.
  */
 
-var express = require('express'),
+var util = require("util"),
+    express = require('express'),
     routes = require('./routes'),
     mongoose = require('mongoose'),
+    mongoStore = require('connect-mongodb'),
     db,
     User,
     Document;
@@ -18,6 +20,8 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ store: mongoStore(app.set('db-uri')), secret: 'topsecret' }));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -41,6 +45,24 @@ app.configure('test', function() {
 // Db
 db = mongoose.connect(app.set('db-uri'));
 app.Document = Document = require('./models.js').Document(db);
+app.User = User = require('./models.js').User(db);
+
+
+// 로그인 확인
+function loadUser(req, res, next) {
+  if (req.session.user_id) {
+    User.findById(req.session.user_id, function(user) {
+      if (user) {
+        req.currentUser = user;
+        next();
+      } else {
+        res.redirect('/sessions/new');
+      }
+    });
+  } else {
+    res.redirect('/sessions/new');
+  }
+}
 
 
 // Routes
@@ -49,7 +71,7 @@ app.get('/', function(req, res) {
 });
 
 // List
-app.get('/documents.:format?', function(req, res) {
+app.get('/documents.:format?', loadUser, function(req, res) {
   Document.find({}, function(err, documents) {
     switch (req.params.format) {
       case 'json':
@@ -66,7 +88,7 @@ app.get('/documents.:format?', function(req, res) {
   });
 });
 
-app.get('/documents/:id.:format?/edit', function(req, res) {
+app.get('/documents/:id.:format?/edit', loadUser, function(req, res) {
     Document.findById(req.params.id, function(err, d) {
         res.render('documents/edit.jade', {
             locals: { d: d }
@@ -74,14 +96,14 @@ app.get('/documents/:id.:format?/edit', function(req, res) {
     });
 });
 
-app.get('/documents/new', function(req, res) {
+app.get('/documents/new', loadUser, function(req, res) {
     res.render('documents/new.jade', {
         locals: { d: new Document() }
     });
 });
  
 // Create
-app.post('/documents.:format?', function(req, res) {
+app.post('/documents.:format?', loadUser, function(req, res) {
   var document = new Document(req.body['document']);
   document.save(function(err) {
     switch (req.params.format) {
@@ -95,7 +117,7 @@ app.post('/documents.:format?', function(req, res) {
 });
  
 // Read
-app.get('/documents/:id.:format?', function(req, res) {
+app.get('/documents/:id.:format?', loadUser, function(req, res) {
   Document.findById(req.params.id, function(err, d) {
     switch (req.params.format) {
       case 'json':
@@ -110,7 +132,7 @@ app.get('/documents/:id.:format?', function(req, res) {
 });
  
 // Update
-app.put('/documents/:id.:format?', function(req, res) {
+app.put('/documents/:id.:format?', loadUser, function(req, res) {
   Document.findById(req.body.document.id, function(err, d) {
     d.title = req.body.document.title;
     d.data = req.body.document.data;
@@ -127,7 +149,7 @@ app.put('/documents/:id.:format?', function(req, res) {
 });
  
 // Delete
-app.del('/documents/:id.:format?', function(req, res) {
+app.del('/documents/:id.:format?', loadUser, function(req, res) {
   Document.findById(req.params.id, function(err, d) {
     d.remove(function(err) {
       switch (req.params.format) {
@@ -141,6 +163,66 @@ app.del('/documents/:id.:format?', function(req, res) {
   });
 });
 
+// 사용자 추가
+app.get('/users/new', function(req, res) {
+  res.render('users/new.jade', {
+    locals: { user: new User() }
+  })
+});
+
+app.post('/users.:format?', function(req, res) {
+  var user = new User(req.body.user);
+
+  function userSaveFailed() {
+    res.render('users/new.jade', {
+      locals: { user: user }
+    });
+  }
+
+  // 문서 저장
+  user.save(function(err) {
+    if(err) return userSaveFailed();
+
+    switch(req.params.format) {
+      case 'json':
+        res.send(user.__doc);
+        break;
+
+      default:
+        req.session.user_id = user.id;  // 세션에 user id 추가
+        res.redirect('/documents');
+    }
+  });
+});
+
+// Login 페이지
+app.get('/sessions/new', function(req, res) {
+  res.render('sessions/new.jade', {
+    locals: { user: new User() }
+  });
+});
+ 
+// Login 처리
+app.post('/sessions', function(req, res) {
+  User.findOne({ email: req.body.user.email }, function(err, user){
+    if(user && user.authenticate(req.body.user.password)) {
+      req.session.user_id = user.email;
+      res.redirect('/documents');
+    } else {
+      // Login 페이지로
+      res.redirect('/sessions/new');
+    }
+  })
+});
+ 
+ // 세션 제거
+app.del('/sessions', loadUser, function(req, res) {
+  if (req.session) {
+    req.session.destroy(function() {});
+  }
+  res.redirect('/sessions/new');
+});
+
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
--- models.js
+++ models.js
@@ -1,8 +1,12 @@
-var mongoose = require('mongoose');
+var util = require("util"),
+    mongoose = require('mongoose'),
+    crypto = require('crypto');
 
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
+
+//Document
 Document = new Schema({
   'title': {type: String, index: true},
   'data': String,
@@ -13,9 +17,64 @@ Document.virtual('id').get(function() {
   return this._id.toHexString();
 });
 
-// 몽구스에게 만들어진 문서 알려주기
-mongoose.model('Document', Document);
 
+//User
+function validatePresenceOf(value) {
+  return value && value.length;
+}
+
+User = new Schema({
+  'email': {
+    type: String,
+    validate: [validatePresenceOf, 'an email is required'],
+    index: { unique: true } // 이메일 주소의 사용자가 오직 한명임을 확실히 하기 위해 unique 사용
+  },
+  'hashed_password': String,
+  'salt': String
+});
+
+User.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+
+User.virtual('password').set(function(password) {
+  this._password = password;
+  this.salt = this.makeSalt();
+  this.hashed_password = this.encryptPassword(password);
+})
+.get(function() { return this._password; });
+
+User.method('authenticate', function(plainText) {
+  return this.encryptPassword(plainText) === this.hashed_password;
+});
+
+User.method('makeSalt', function() {
+  return Math.round((new Date().valueOf() * Math.random())) + '';
+});
+
+User.method('encryptPassword', function(password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+});
+
+// Mongoose 미들웨어 정의 - save 호출시에 발생
+User.pre('save', function(next) {
+  if (!validatePresenceOf(this.password)) {
+    next(new Error('Invalid password'));
+  } else {
+    next();
+  }
+});
+
+
+// 모델 생성
+mongoose.model('Document', Document);
+mongoose.model('User', User);
+
+
+//모델 접근
 exports.Document = function(db) {
-  return db.model('Document');  // ‘Document’라는 문서모델에 접근한다.
+  return db.model('Document');
+};
+exports.User = function(db) {
+  return db.model('User');
 };
